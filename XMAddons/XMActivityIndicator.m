ce575cb18a55bd140f1de5b2fcbddf1c4bbf5215
Jayakrishnan M <javajk2537@gmail.com>
2017-06-30 12:08:41 +0530

    added license to files

--- XMAddons/XMActivityIndicator.m
+++ XMAddons/XMActivityIndicator.m
@@ -1,11 +1,32 @@
 //
 //  XMActivityIndicator.m
 //
+//     MIT License
+//    
+//     Copyright (c) 2017 bindurs
+//    
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//    
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//    
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
 
 #import "XMActivityIndicator.h"
 
 @implementation XMActivityIndicator{
-    
+
     UIActivityIndicatorView *activity;
 }
 @synthesize activityType,activityIndicatorSize;
@@ -15,7 +36,7 @@
     activityIndicatorSize = @100;
 
     self = [super initWithFrame:frame];
-    
+
     if (self) {
         [self setupActivity];
     }
@@ -23,9 +44,9 @@
 }
 
 - (id)init{
-    
+
     activityType = BRSACTIVITYLARGE;
- 
+
     if (activityType == BRSACTIVITYSMALL) {
         self = [super initWithFrame:CGRectMake(0,0, [activityIndicatorSize floatValue], [activityIndicatorSize floatValue])];
     } else {
@@ -40,7 +61,7 @@
 }
 
 - (void) setupActivity{
-    
+
     self.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
     //    self.layer.cornerRadius = 5;
     self.layer.masksToBounds = YES;
@@ -53,7 +74,7 @@
 }
 
 -(void)setActivityType:(Activity_Type)type {
-    
+
     activityType = type;
     if (type == BRSACTIVITYSMALL) {
         self.frame=CGRectMake(0,0, [activityIndicatorSize floatValue], [activityIndicatorSize floatValue]);
@@ -69,12 +90,12 @@
 }
 
 -(void)setActivityIndicatorSize:(NSNumber *)size {
-    
+
     activityIndicatorSize = size;
     if (activityType == BRSACTIVITYSMALL) {
         self.frame=CGRectMake(0,0, [size floatValue], [size floatValue]);
         activity.center = self.center;
-        
+
     }
 }
 
@@ -83,7 +104,7 @@
 }
 
 -(void)setActivityCornerRadius:(NSInteger)cornerRadius {
-    
+
     if (activityType == BRSACTIVITYSMALL) {
          self.layer.cornerRadius = cornerRadius;
     }
@@ -97,15 +118,15 @@
 }
 
 - (void) activityShow: (BOOL) showStatus{
-    
+
     if (showStatus) {
-        
+
         [self.superview bringSubviewToFront:self];
         self.hidden =NO;
         [activity startAnimating];
-        
+
     } else {
-        
+
         self.hidden =YES;
         [activity stopAnimating];
     }
